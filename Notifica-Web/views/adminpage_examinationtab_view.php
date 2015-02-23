24fde8a0f79a661d76bc042df0f4efb64c7a8b2d
Bibek Dahal <bibekdahal.bd16@gmail.com>
2015-02-23 14:35:43 +0545

    Revert "Develop"

--- Notifica-Web/views/adminpage_examinationtab_view.php
+++
@@ -1,9 +0,0 @@
-<div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">List Examination Schedule <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Manage Examination Schedule</a></li>
-        </div>
-    </div>
-</div>
