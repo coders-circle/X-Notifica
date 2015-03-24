8c7d5a522037ef9a49a802c962f3da6b040e2da0
frozenhelium <fren.ankit@gmail.com>
2015-03-24 19:23:19 +0545

    added batch and faculty to the routine form

--- Notifica-Web/views/adminpage_routinetab_view.php
+++ Notifica-Web/views/adminpage_routinetab_view.php
@@ -1,4 +1,49 @@
 <div class="container">
+    <div class="row">
+        <div class="col-md-12 main">
+            <h1 class="page-header">Add New Routine</h1>
+            <form class="form-adduser" action="index.php?page=adminpage&amp;tab=routine" method="post" name="registration_form" role="form">
+                <input type='number' placeholder="Batch" name="batch" id="batch" required>
+                <select placeholder="Select a faculty" name="faculty" id ="faculty" required>
+                    <?php
+                        $user = $GLOBALS['g_user'];
+                        $result = $user->GetFaculties();
+                        while($row = $result->fetch_assoc()){
+                            echo '<option value = '.$row["id"].'">'.$row["name"].'</option>';
+                        }
+                    ?>
+                </select>
+                <ul class="nav nav-tabs " id="days-tab"  role="tablist">
+                    <li role="presentation" class="active"><a href="#sun" id="sunday-tab" role="tab" data-toggle="tab" aria-controls="sunday" aria-expanded="true">Sunday</a></li>
+                    <li role="presentation"><a href="#mon" role="tab" id="monday-tab" data-toggle="tab" aria-controls="monday">Monday</a></li>
+                    <li role="presentation"><a href="#tue" role="tab" id="tuesday-tab" data-toggle="tab" aria-controls="tuesday">Tuesday</a></li>
+                    <li role="presentation"><a href="#wed" role="tab" id="wednesday-tab" data-toggle="tab" aria-controls="wednesday">Wednesday</a></li>
+                    <li role="presentation"><a href="#thu" role="tab" id="thursday-tab" data-toggle="tab" aria-controls="thursday">Thursday</a></li>
+                    <li role="presentation"><a href="#fri" role="tab" id="friday-tab" data-toggle="tab" aria-controls="friday">Friday</a></li>
+                    <li role="presentation"><a href="#sat" role="tab" id="saturday-tab" data-toggle="tab" aria-controls="saturday">Saturday</a></li>
+                </ul>
+
+                <div id="days-tab-contents" class="tab-content">
+                    <?php
+                        $divlabels = array("sunday-tab", "monday-tab", "tuesday-tab", "wednesday-tab", "thursday-tab", "friday-tab", "saturday-tab");
+                        $divids = array("sun", "mon", "tue", "wed", "thu", "fri", "sat");
+                        $innerdivids = array("sun_routine", "mon_routine", "tue_routine", "wed_routine", "thu_routine", "fri_routine", "sat_routine");
+                        $count = 0;
+                        while($count < 7){
+                            if($count == 0) echo '<div role="tabpanel" class="tab-pane fade in active" id="'.$divids[$count].'" aria-labelledBy="'.$divlabels[$count].'">';
+                            else echo '<div role="tabpanel" class="tab-pane fade in" id="'.$divids[$count].'" aria-labelledBy="'.$divlabels[$count].'">';
+                            echo '<div id = "'.$innerdivids[$count].'">';
+                            echo '</div>';
+                            echo '<button type="button" class="btn btn-default btn-lg" onClick=\'AddRow("#'.$innerdivids[$count].'");\'>
+                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add a period
+                            </button></div>';
+                            ++$count;
+                        }
+                    ?>
+                </div>
+            </form>
+        </div>
+    </div>
     <div id = "period" style = "visibility:hidden; ">
         <div class="row">
             <div class="col-md-4">
@@ -31,42 +76,6 @@
                     ?>
                 </select>
             </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12 main">
-            <h1 class="page-header">Add New Routine</h1>
-            <form class="form-adduser" action="index.php?page=adminpage&amp;tab=routine" method="post" name="registration_form" role="form">
-                <ul class="nav nav-tabs " id="days-tab"  role="tablist">
-                    <li role="presentation" class="active"><a href="#sun" id="sunday-tab" role="tab" data-toggle="tab" aria-controls="sunday" aria-expanded="true">Sunday</a></li>
-                    <li role="presentation"><a href="#mon" role="tab" id="monday-tab" data-toggle="tab" aria-controls="monday">Monday</a></li>
-                    <li role="presentation"><a href="#tue" role="tab" id="tuesday-tab" data-toggle="tab" aria-controls="tuesday">Tuesday</a></li>
-                    <li role="presentation"><a href="#wed" role="tab" id="wednesday-tab" data-toggle="tab" aria-controls="wednesday">Wednesday</a></li>
-                    <li role="presentation"><a href="#thu" role="tab" id="thursday-tab" data-toggle="tab" aria-controls="thursday">Thursday</a></li>
-                    <li role="presentation"><a href="#fri" role="tab" id="friday-tab" data-toggle="tab" aria-controls="friday">Friday</a></li>
-                    <li role="presentation"><a href="#sat" role="tab" id="saturday-tab" data-toggle="tab" aria-controls="saturday">Saturday</a></li>
-                </ul>
-
-                <div id="days-tab-contents" class="tab-content">
-                    <?php
-                        $divlabels = array("sunday-tab", "monday-tab", "tuesday-tab", "wednesday-tab", "thursday-tab", "friday-tab", "saturday-tab");
-                        $divids = array("sun", "mon", "tue", "wed", "thu", "fri", "sat");
-                        $innerdivids = array("sun_routine", "mon_routine", "tue_routine", "wed_routine", "thu_routine", "fri_routine", "sat_routine");
-                        $count = 0;
-                        while($count < 7){
-                            if($count == 0) echo '<div role="tabpanel" class="tab-pane fade in active" id="'.$divids[$count].'" aria-labelledBy="'.$divlabels[$count].'">';
-                            else echo '<div role="tabpanel" class="tab-pane fade in" id="'.$divids[$count].'" aria-labelledBy="'.$divlabels[$count].'">';
-                            echo '<div id = "'.$innerdivids[$count].'">';
-                            echo '</div>';
-                            echo '<button type="button" class="btn btn-default btn-lg" onClick=\'AddRow("#'.$innerdivids[$count].'");\'>
-                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add a period
-                            </button></div>';
-                            ++$count;
-                        }
-                    ?>
-
-                </div>
-            </form>
         </div>
     </div>
 </div>
