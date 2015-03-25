fc9f8d9832b003883acfbc2e6fabca8f119c33c4
bibek <bibekdahal.bd16@gmail.com>
2015-03-25 12:23:31 +0545

    Routine Post Function Managed

--- Notifica-Web/controls/adminpage_control.php
+++ Notifica-Web/controls/adminpage_control.php
@@ -51,35 +51,14 @@ else if($tabID == 'courses'){
 }
 
 else if($tabID == 'routine'){
+    $dayarr = [ 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat' ];
     if (isset($_POST['faculty']) && isset($_POST['batch'])) {
-        $srs = $_POST['#sun_routinestart'];
-        $sre = $_POST['#sun_routineend'];
-        $sru = $_POST['#sun_routinesubject'];
-        $srt = $_POST['#sun_routineteacher'];
-        $mrs = $_POST['#mon_routinestart'];
-        $mre = $_POST['#mon_routineend'];
-        $mru = $_POST['#mon_routinesubject'];
-        $mrt = $_POST['#mon_routineteacher'];
-        $trs = $_POST['#tue_routinestart'];
-        $tre = $_POST['#tue_routineend'];
-        $tru = $_POST['#tue_routinesubject'];
-        $trt = $_POST['#tue_routineteacher'];
-        $wrs = $_POST['#wed_routinestart'];
-        $wre = $_POST['#wed_routineend'];
-        $wru = $_POST['#wed_routinesubject'];
-        $wrt = $_POST['#wed_routineteacher'];
-        $ttrs = $_POST['#thu_routinestart'];
-        $ttre = $_POST['#thu_routineend'];
-        $ttru = $_POST['#thu_routinesubject'];
-        $ttrt = $_POST['#thu_routineteacher'];
-        $frs = $_POST['#fri_routinestart'];
-        $fre = $_POST['#fri_routineend'];
-        $fru = $_POST['#fri_routinesubject'];
-        $frt = $_POST['#fri_routineteacher'];
-        $strs = $_POST['#sat_routinestart'];
-        $stre = $_POST['#sat_routineend'];
-        $stru = $_POST['#sat_routinesubject'];
-        $strt = $_POST['#sat_routineteacher'];
+        for ($day = 0; $day < 7; ++$day) {
+            $srs[$day] = $_POST['#'.$dayarr[$day].'_routinestart'];
+            $sre[$day] = $_POST['#'.$dayarr[$day].'_routineend'];
+            $sru[$day] = $_POST['#'.$dayarr[$day].'_routinesubject'];
+            $srt[$day] = $_POST['#'.$dayarr[$day].'_routineteacher'];
+        }
 
         $faculty_id = intval($_POST['faculty']);
         $batch = intval($_POST['batch']);
@@ -88,75 +67,25 @@ else if($tabID == 'routine'){
 
         $elements = array();
         $count = 0;
-        for ($i=0; $i<count($sru); $i++) {
+        for ($day=0; $day<7; $day++) {
+        for ($i=0; $i<count($sru[$day]); $i++) {
             $element = array();
-            $element['subject_id']  = intval($sru[$i]);
-            $element['teacher_id']  = intval($srt[$i]);
-            $element['day'] = 0;
-            $element['starttime']   = GetTime($srs[$i]);
-            $element['endtime']     = GetTime($sre[$i]);
+            $element['subject_id']  = intval($sru[$day][$i]);
+            $element['teacher_id']  = intval($srt[$day][$i]);
+            $element['day'] = $day;
+            $element['starttime']   = GetTime($srs[$day][$i]);
+            $element['endtime']     = GetTime($sre[$day][$i]);
             $elements[$count++] = $element;
             $element = array();
         }
-        for ($i=0; $i<count($mru); $i++) {
-            $element['subject_id']  = intval($mru[$i]);
-            $element['teacher_id']  = intval($mrt[$i]);
-            $element['day'] = 1;
-            $element['starttime']   = GetTime($mrs[$i]);
-            $element['endtime']     = GetTime($mre[$i]);
-            $elements[$count++] = $element;
         }
-        for ($i=0; $i<count($tru); $i++) {
-            $element = array();
-            $element['subject_id']  = intval($tru[$i]);
-            $element['teacher_id']  = intval($trt[$i]);
-            $element['day'] = 2;
-            $element['starttime']   = GetTime($trs[$i]);
-            $element['endtime']     = GetTime($tre[$i]);
-            $elements[$count++] = $element;
-        }
-        for ($i=0; $i<count($wru); $i++) {
-            $element = array();
-            $element['subject_id']  = intval($wru[$i]);
-            $element['teacher_id']  = intval($wrt[$i]);
-            $element['day'] = 3;
-            $element['starttime']   = GetTime($wrs[$i]);
-            $element['endtime']     = GetTime($wre[$i]);
-            $elements[$count++] = $element;
-        }
-        for ($i=0; $i<count($ttru); $i++) {
-            $element = array();
-            $element['subject_id']  = intval($ttru[$i]);
-            $element['teacher_id']  = intval($ttrt[$i]);
-            $element['day'] = 4;
-            $element['starttime']   = GetTime($ttrs[$i]);
-            $element['endtime']     = GetTime($ttre[$i]);
-            $elements[$count++] = $element;
-        }
-        for ($i=0; $i<count($fru); $i++) {
-            $element = array();
-            $element['subject_id']  = intval($fru[$i]);
-            $element['teacher_id']  = intval($frt[$i]);
-            $element['day'] = 5;
-            $element['starttime']   = GetTime($frs[$i]);
-            $element['endtime']     = GetTime($fre[$i]);
-            $elements[$count++] = $element;
-        }
-        for ($i=0; $i<count($stru); $i++) {
-            $element = array();
-            $element['subject_id']  = intval($stru[$i]);
-            $element['teacher_id']  = intval($strt[$i]);
-            $element['day'] = 6;
-            $element['starttime']   = GetTime($strs[$i]);
-            $element['endtime']     = GetTime($stre[$i]);
-            $elements[$count++] = $element;
-        }
+        
    
- //       for ($i=0; $i<$count; $i++) {
- //           $arr = $elements[$i];
- //           foreach($arr as $key => $value)
- //               echo $key . " = ". $value . "<br/>";
- //       }      
+//        for ($i=0; $i<$count; $i++) {
+//            $arr = $elements[$i];
+//            foreach($arr as $key => $value)
+//                echo $key . " = ". $value . "<br/>";
+//        }      
         $user->AddRoutine($faculty_id, $batch, $group, 0, 24*60, $elements);
     }
     $adminPage->SetActiveTab(4);
