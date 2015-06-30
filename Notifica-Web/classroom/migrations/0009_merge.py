746826c1336ef3ec161877b4a1a46f8b77b7cc59
frozenhelium <fren.ankit@gmail.com>
2015-07-01 00:14:30 +0545

    templates created for authority

---
+++ Notifica-Web/classroom/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('classroom', '0008_auto_20150630_1758'),
+        ('classroom', '0008_auto_20150630_1927'),
+    ]
+
+    operations = [
+    ]
