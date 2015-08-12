4c192e213b96dc72551c2a4206086264076e564d
frozenhelium <fren.ankit@gmail.com>
2015-08-13 02:16:17 +0545

    Removed unimplemented contents + carousel routine view for small device

--- Notifica-Web/static/js/common.js
+++ Notifica-Web/static/js/common.js
@@ -100,41 +100,6 @@ if ( typeof define === 'function' && define.amd ) {
 
 })( window );
 
-var cbpAnimatedHeader = (function() {
-
-	var docElem = document.documentElement,
-		header = document.querySelector( '.navbar-fixed-top' ),
-		didScroll = false,
-		changeHeaderOn = 200;
-
-	function init() {
-		window.addEventListener( 'scroll', function( event ) {
-			if( !didScroll ) {
-				didScroll = true;
-				setTimeout( scrollPage, 250 );
-			}
-		}, false );
-	}
-
-	function scrollPage() {
-		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove( header, 'navbar-shrink' );
-		}
-		didScroll = false;
-	}
-
-	function scrollY() {
-		return window.pageYOffset || docElem.scrollTop;
-	}
-
-	init();
-
-})();
-
 
 function getCookie(name) {
     var cookieValue = null;
--- Notifica-Web/static/js/home.js
+++ Notifica-Web/static/js/home.js
@@ -3,3 +3,38 @@
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
+
+
+var cbpAnimatedHeader = (function() {
+	var docElem = document.documentElement,
+		header = document.querySelector( '.navbar-fixed-top' ),
+		didScroll = false,
+		changeHeaderOn = 500;
+
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 250 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( header, 'navbar-shrink' );
+		}
+		else {
+			classie.remove( header, 'navbar-shrink' );
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+})();
